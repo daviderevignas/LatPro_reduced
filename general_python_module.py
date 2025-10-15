7b54f5e9922cb8ccfe78bf60df67bd02de8131e4
Davide Revignas <davide.revignas@gmail.com>
2025-10-15 15:31:13 +0200

    loading functions added

--- general_python_module.py
+++ general_python_module.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import os
 
 
 
@@ -15,9 +16,35 @@ import sys
 
 
 
+##############################
+#     loading functions      #
+##############################
 
 
+def load_hamiltonian_walks_contacts():
+    tmp_hamiltonian_walks_contacts=np.loadtxt(\
+        os.path.join("data/","hamiltonian_walks_contacts.csv"),delimiter=',',dtype=np.int16)
+    hamiltonian_walks_contacts=[]
+    for row in tmp_hamiltonian_walks_contacts:
+        hamiltonian_walks_contacts.append([])
+        for ii in range(len(row)//2):
+            hamiltonian_walks_contacts[-1].append([row[2*ii],row[2*ii+1]])
+    hamiltonian_walks_contacts=np.array(hamiltonian_walks_contacts,dtype=np.int16)
+    del tmp_hamiltonian_walks_contacts
+    return hamiltonian_walks_contacts
 
+def load_hamiltonian_walks_bond_vectors():
+    hamiltonian_walks_bond_vectors=np.loadtxt(\
+        os.path.join("data/","hamiltonian_walks_bond_vectors.csv"),delimiter=',',dtype=np.int16)
+    return hamiltonian_walks_bond_vectors
+
+def load_MJ_tab_6():
+    tmp_MJ_tab_6=np.loadtxt(os.path.join("data/","MJ_tab_6.csv"),delimiter=',')
+    MJ_tab_6=np.zeros((20,20))
+    for ii in range(len(tmp_MJ_tab_6)):
+        MJ_tab_6[int(tmp_MJ_tab_6[ii,0])-1, int(tmp_MJ_tab_6[ii,1])-1] = tmp_MJ_tab_6[ii,2]
+    del tmp_MJ_tab_6
+    return MJ_tab_6
 
 
 
